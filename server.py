fffdfdebd9be343b6cd20d67c5eb189f156422fa
Obi Emmanuel <anayo@uab.edu>
2020-07-09 01:58:38 -0500

    Add redesisign home page with image carousel

--- server.py
+++ server.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
 @app.route('/home')
+@app.route('/')
 def homepage():
-	return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/login')
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
 
-app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True)
