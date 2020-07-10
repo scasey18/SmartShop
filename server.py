16547f49a4fbc100423168c564e0aaf9ceb98afe
Obi Emmanuel <anayo@uab.edu>
2020-07-10 17:31:13 -0500

    add login, cart,and signup page

--- server.py
+++ server.py
@@ -14,5 +14,15 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/cart')
+def getCart():
+    return render_template('cart.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
