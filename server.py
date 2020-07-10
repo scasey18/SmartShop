e858163d9e64e51983163b892e742ff7c53c483f
Samuel Casey <scasey18@uab.edu>
2020-07-10 17:03:59 -0500

    Sample for Database

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+
+db_name = 'sample.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
 @app.route('/')
 @app.route('/home')
