054fa9849ea3f286a50c55e0e1caf77bde14047e
Andrew P Balfour <abalfour@uab.edu>
2020-07-13 21:40:34 -0500

    store about and contact pages

--- server.py
+++ server.py
@@ -23,6 +23,17 @@ def signup():
 def getCart():
     return render_template('cart.html')
 
+@app.route('/store')
+def store():
+    return render_template('store.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
