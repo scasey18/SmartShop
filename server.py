593f64b6863236a28eba937abd41143ed176f88b
Obi Emmanuel <anayo@uab.edu>
2020-07-21 22:14:16 -0500

    added 36 product images, bags, shoes and clothing

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
-from flask_login import LoginManager, UserMixin, current_user, login_user
+from flask_login import LoginManager, current_user, login_user, logout_user
+
+from flask_wtf.file import FileField
+from wtforms import SubmitField
+from flask_wtf import Form
+import sqlite3
+
+from dbtools import *
 
 app = Flask(__name__)
 
@@ -11,62 +16,62 @@ db_name = 'sample.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-db = SQLAlchemy(app)
 
-class User(UserMixin, db.Model):
-	__tablename__ = 'Customers'
-	custID = db.Column(db.Integer, primary_key=True)
-	fName = db.Column(db.String(50))
-	lName = db.Column(db.String(50))
-	emailAdr = db.Column(db.String(50), unique=True)
-	password = db.Column(db.String(50))
-	
-	def __init__(self, fname,lname,emailAdr,password):
-		self.fName = fname
-		self.lName = lname
-		self.emailAdr = emailAdr
-		self.password = password
-	
-	def get_id(self):
-           return (self.custID)
+db.init_app(app)
 
-class Product(db.Model):
-	prodID = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(50))
-	price = db.Column(db.Numeric(scale=2))
-	curinv = db.Column(db.Integer)
-	desc = db.Column(db.String(124))
-	
-	def __init__(self, name, price, curinv, desc):
-		self.name = name
-		self.price = price
-		self.curinv = curinv
-		self.desc = desc
+with app.app_context():
+    db.create_all()
 
-db.create_all()
+app.secret_key = 'CS421'
 
-app.secret_key='CS421'
 
 @login.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-@app.route('/home')
-@app.route('/')
-def homepage():
-	if current_user.is_authenticated:
-		return render_template('home.html', data=current_user)
-	return render_template('home.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	if request.method == "POST":
-			req = dict(request.form)
-			user = User.query.filter_by(emailAdr=req['uname']).first()
-			if user.password == req['pwd']:
-				login_user(user)
-				return redirect(url_for('homepage'))
-	return render_template('login.html')
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
+
+
+@app.route('/')
+@app.route('/home')
+def homepage():
+    if current_user.is_authenticated:
+        return render_template('home.html', user=current_user)
+    return render_template('home.html')
+
+
+# # Upload product into the database
+# @app.route('/upload', methods=["GET", "POST"])
+# def upload_product():
+#     form = UploadForm()
+#     if request.method == "POST":
+#         if form.validate_on_submit():
+#             file_name = form.file.data
+#             insert_product(name=file_name.filename, price=59.99, curinv=1, desc="Old survival backpack")
+#             return render_template('upload_product.html', form=form)
+#     return render_template('upload_product.html', form=form)
+#
+#
+# class UploadForm(Form):
+#     file = FileField()
+#     submit = SubmitField("submit")
+#
+#
+# def insert_product(name, price, curinv, desc):
+#     dbConnect = sqlite3.connect(db_name)
+#     cursor = dbConnect.cursor()
+#     cursor.execute(
+#         "CREATE TABLE  IF NOT EXISTS Product(ProdID INTEGER PRIMARY KEY, name String, price Numeric, curinv Integer, desc String)")
+#     cursor.execute("INSERT INTO Product (name, price, curinv, desc) VALUES (?,?,?,?)",
+#                    (name, price, curinv, desc))
+#     dbConnect.commit()
+#     cursor.close()
+#     dbConnect.close()
+
 
 @app.route('/signup')
 def signup():
@@ -77,17 +82,21 @@ def signup():
 def getCart():
     return render_template('cart.html')
 
+
 @app.route('/store')
 def store():
     return render_template('store.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 @app.route('/thankyou')
 def signup_thankyou():
@@ -108,41 +117,38 @@ def sales_thankyou():
 def product():
     return render_template('product.html')
 
-@app.route('/admin', methods=['GET','POST'])
-def admin():
-	if request.method == 'POST':
-		req = dict(request.form)
-		print(req)
-		if "Customer" in req:
-			if req["Customer"] == "Add":
-				cust = User(req['fname'],req['lname'],req['email'],req['pwd'])
-				db.session.add(cust)
-			elif req["Customer"] == "Remove":
-				res = User.query.filter_by(custID=req["id"]).first()
-				db.session.delete(res)
-			elif req["Customer"] == "Update":
-				res = User.query.filter_by(custID=req["id"]).first()
-				res.fName = req['fname']
-				res.lName = req['lname']
-				res.emailAdr = req['email']
-				if str(req['pwd']) != '':
-					res.password = req['pwd']
-		elif "Product" in req:
-			if req["Product"] == "Add":
-				cust = Product(req['name'],req['price'],req['stock'],req['desc'])
-				db.session.add(cust)
-			elif req["Product"] == "Remove":
-				res = Product.query.filter_by(prodID=req["id"]).first()
-				db.session.delete(res)
-			elif req["Product"] == "Update":
-				res = Product.query.filter_by(prodID=req["Pid"]).first()
-				res.name = req['name']
-				res.price = req['price']
-				res.curinv = req['stock']
-				if req['desc'] != '':
-					res.desc = req['desc']
-		db.session.commit()
-	return render_template("admin.html", customer=User.query.all(),products=Product.query.all())
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        req = dict(request.form)
+        user = User.query.filter_by(emailAdr=req['uname']).first()
+        if user.password == req['pwd']:
+            login_user(user)
+            return redirect(url_for('homepage'))
+    return render_template('login.html')
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        req = dict(request.form)
+        print(req)
+        if "Customer" in req:
+            if req["Customer"] == "Add":
+                addCustomer(req['fname'], req['lname'], req['email'], req['pwd'])
+            elif req["Customer"] == "Remove":
+                deleteCustomer(req['id'])
+            elif req["Customer"] == "Update":
+                updateProduct(req['id'], req['fname'], req['lname'], req['email'], req['pwd'])
+        elif "Product" in req:
+            if req["Product"] == "Add":
+                addProduct(req['name'], req['price'], req['stock'], req['desc'])
+            elif req["Product"] == "Remove":
+                deleteProduct(req['Pid'])
+            elif req["Product"] == "Update":
+                updateProduct(req['Pid'], req['name'], req['price'], req['stock'], req['desc'])
+    return render_template("admin.html", customer=User.query.all(), products=Product.query.all())
+
+
+app.run()
