6adab7cebd3cc7a5c367d6276ee72e94781e4965
Andrew P Balfour <abalfour@uab.edu>
2020-07-28 18:11:49 -0500

    redesign changes and product

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 db.init_app(app)
 
 with app.app_context():
-	db.create_all()
+    db.create_all()
 
 app.secret_key = 'CS421'
 
@@ -32,114 +32,131 @@ def logout():
     logout_user()
     return redirect(url_for('homepage'))
 
+
 @app.route('/')
 @app.route('/home')
 def homepage():
-	if current_user.is_authenticated:
-		return render_template('home.html', user=current_user)
-	return render_template('home.html')
+    if current_user.is_authenticated:
+        return render_template('home.html', user=current_user)
+    return render_template('home.html')
+
 
 @app.route('/cart')
 def getCart():
-	if current_user.is_authenticated:
-		return render_template('cart.html', user=current_user)
-	return render_template('cart.html')
+    if current_user.is_authenticated:
+        return render_template('cart.html', user=current_user)
+    return render_template('cart.html')
+
 
 @app.route('/store')
 def store():
-	if current_user.is_authenticated:
-		return render_template('store.html', user=current_user, products=Product.query.all())
-	return render_template('store.html', products=Product.query.all())
+    if current_user.is_authenticated:
+        return render_template('store.html', user=current_user, products=Product.query.all())
+    return render_template('store.html', products=Product.query.all())
+
 
 @app.route('/about')
 def about():
-	if current_user.is_authenticated:
-		return render_template('about.html', user=current_user)
-	return render_template('about.html')
+    if current_user.is_authenticated:
+        return render_template('about.html', user=current_user)
+    return render_template('about.html')
+
 
 @app.route('/contact')
 def contact():
-	if current_user.is_authenticated:
-		return render_template('contact.html', user=current_user)
-	return render_template('contact.html')
+    if current_user.is_authenticated:
+        return render_template('contact.html', user=current_user)
+    return render_template('contact.html')
+
 
 @app.route('/thankyou')
 def signup_thankyou():
-	return render_template('signup_thankyou.html')
+    return render_template('signup_thankyou.html')
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-	if current_user.is_authenticated:
-		return redirect(url_for("homepage"))
-	error = None
-	if request.method == "POST":
-		req = dict(request.form)
-		user = User.query.filter_by(emailAdr=req['email']).first()
-		if (user != None):
-			error = "Email address already in use"
-		elif (req['pwd'] != req['cpwd']):
-			error = "Passwords entered are not the same"
-		else:
-			addCustomer(req['fName'], req['lName'], req['email'], req['pwd'])
-			return redirect(url_for('signup_thankyou'))
-		return render_template('signup.html', error=error)
-	return render_template('signup.html')
+    if current_user.is_authenticated:
+        return redirect(url_for("homepage"))
+    error = None
+    if request.method == "POST":
+        req = dict(request.form)
+        user = User.query.filter_by(emailAdr=req['email']).first()
+        if (user != None):
+            error = "Email address already in use"
+        elif (req['pwd'] != req['cpwd']):
+            error = "Passwords entered are not the same"
+        else:
+            addCustomer(req['fName'], req['lName'], req['email'], req['pwd'])
+            return redirect(url_for('signup_thankyou'))
+        return render_template('signup.html', error=error)
+    return render_template('signup.html')
+
 
 @app.route('/checkout')
 def checkout():
-	if current_user.is_authenticated:
-		return render_template('checkout.html', user=current_user)
-	return render_template('checkout.html')
+    if current_user.is_authenticated:
+        return render_template('checkout.html', user=current_user)
+    return render_template('checkout.html')
 
 
 @app.route('/thank_you')
 def sales_thankyou():
-	if current_user.is_authenticated:
-		return render_template('sales_thankyou.html', user=current_user)
-	return render_template('sales_thankyou.html')
+    if current_user.is_authenticated:
+        return render_template('sales_thankyou.html', user=current_user)
+    return render_template('sales_thankyou.html')
 
 
-@app.route('/product')
+@app.route('/product', methods=['GET', 'POST'])
 def product():
-	if current_user.is_authenticated:
-		return render_template('product.html', user=current_user)
-	return render_template('product.html')
+    jsdata = ""
+    if request.method == 'POST':
+        req = dict(request.form)
+    if current_user.is_authenticated:
+        return render_template('product.html', user=current_user, products=Product.query.all(), productid=req['productid'])
+    return render_template('product.html', products=Product.query.all(), productid=req['productid'])
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('account'))
-	if request.method == "POST":
-		req = dict(request.form)
-		user = User.query.filter_by(emailAdr=req['uname']).first()
-		if (user == None or user.password != req['pwd']):
-			return render_template('login.html', error="Invalid username or password")
-		else:
-			login_user(user)
-			return redirect(url_for('homepage'))
-	return render_template('login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
+    if request.method == "POST":
+        req = dict(request.form)
+        user = User.query.filter_by(emailAdr=req['uname']).first()
+        if (user == None or user.password != req['pwd']):
+            return render_template('login.html', error="Invalid username or password")
+        else:
+            login_user(user)
+            return redirect(url_for('homepage'))
+    return render_template('login.html')
 
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
-	if request.method == 'POST':
-		req = dict(request.form)
-		print(req)
-		if "Customer" in req:
-			if req["Customer"] == "Add":
-				addCustomer(req['fname'],req['lname'],req['email'],req['pwd'])
-			elif req["Customer"] == "Remove":
-				deleteCustomer(req['id'])
-			elif req["Customer"] == "Update":
-				updateCustomer(req['id'],req['fname'],req['lname'],req['email'],req['pwd'])
-		elif "Product" in req:
-			if req["Product"] == "Add":
-				addProduct(req['name'],req['price'],req['stock'],req['desc'])
-			elif req["Product"] == "Remove":
-				deleteProduct(req['Pid'])
-			elif req["Product"] == "Update":
-				updateProduct(req['Pid'],req['name'],req['price'],req['stock'],req['desc'])
-	return render_template("admin.html", customer=User.query.all(),products=Product.query.all())
+    if request.method == 'POST':
+        req = dict(request.form)
+        print(req)
+        if "Customer" in req:
+            if req["Customer"] == "Add":
+                addCustomer(req['fname'], req['lname'],
+                            req['email'], req['pwd'])
+            elif req["Customer"] == "Remove":
+                deleteCustomer(req['id'])
+            elif req["Customer"] == "Update":
+                updateCustomer(req['id'], req['fname'],
+                               req['lname'], req['email'], req['pwd'])
+        elif "Product" in req:
+            if req["Product"] == "Add":
+                addProduct(req['name'], req['price'],
+                           req['stock'], req['desc'])
+            elif req["Product"] == "Remove":
+                deleteProduct(req['Pid'])
+            elif req["Product"] == "Update":
+                updateProduct(req['Pid'], req['name'],
+                              req['price'], req['stock'], req['desc'])
+    return render_template("admin.html", customer=User.query.all(), products=Product.query.all())
+
 
 @app.route('/account')
 def account():
