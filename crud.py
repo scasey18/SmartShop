e9d6f180e54e3e1ae632a291834eaf3cbd52e391
Samuel Casey <scasey18@uab.edu>
2020-07-31 16:58:23 -0500

    Move methods to properly space them out

---
+++ crud.py
@@ -0,0 +1,114 @@
+from tables import *
+
+def removeCustomer(id):
+    res = getCustomer(id)
+    db.session.delete(res)
+    db.session.commit()
+
+def getCustomer(id):
+	return User.query.filter_by(custID=id).first()
+
+def addCustomer(fname, lname, email, pwd):
+    cust = User(fname, lname, email, pwd)
+    db.session.add(cust)
+    db.session.commit()
+
+def updateCustomer(id, fname, lname, email, pwd):
+    res = getCustomer(id)
+    res.fName = fname
+    res.lName = lname
+    res.emailAdr = email
+    if pwd != '':
+        res.password = pwd
+    db.session.commit()
+
+def getRating(prodID):
+	return Rating.query.filter(prodID = prodID).first()
+	
+def getCart(custID):
+	return Cart.query.filter_by(custID=custID).all()
+
+def getOrders(id):
+	return Orders.query.filter(custID=id).all()
+
+def getProduct(id):
+	return Product.query.filter_by(prodID=id).first()
+
+def getAddress(id):
+	return Address.query.filter_by(adrID=id).first()
+
+def addProduct(name, price, stock, desc):
+    cust = Product(name, price, stock, desc)
+    db.session.add(cust)
+    db.session.commit()
+
+def removeProduct(id):
+	res = getProduct(id)
+	db.session.delete(res)
+	db.session.commit()
+
+def updateProduct(id, name,price,stock,desc):
+	res = getProduct(id)
+	res.name = name
+	res.price = price
+	res.curinv = stock
+	if desc != '':
+		res.desc = desc
+	db.session.commit()
+
+def addAddress(street, suite, state, country, zip):
+	adr = Address(street, suite, state, country, zip)
+	db.session.add(adr)
+	db.session.commit()
+
+def removeAddress(id):
+	adr = getAddress(id)
+	db.session.delete(adr)
+	db.session.commit()
+
+def updateAddress(id, street, suite, state, country, zip):
+	adr = getAddress(id)
+	adr.street=street
+	adr.suite=suite
+	adr.state = state
+	adr.country = country
+	adr.zip = zip
+	db.session.commit()
+
+def addOrder(custID, ShipAdrID):
+	ord = Orders(custID, ShipAdrID)
+	db.session.add(ord)
+	db.session.commit()
+
+def removeOrder(id):
+	res = getOrders(id)
+	db.session.delete(res)
+	db.session.commit()
+
+def updateOrder(id, custID, ShipAdrID):
+	res = getOrders(id).first()
+	res.custID = custID
+	res.ShipAdrID = ShipAdrID
+	db.session.commit()
+	
+def addRating(custID,prodID,rating,comment=""):
+	res = Rating.query.filter(custID=custID, prodID = prodID).all()
+	if res == None:
+		db.session.add(Rating(custID,prodID,rating,comment))
+		db.session.commit()
+		setProdRating(prodID)
+		db.session.commit()
+
+def updateRating(custID,prodID,rating,comment=""):
+	res = Rating.query.filter(custID=custID, prodID = prodID).all()
+	if res != None:
+		res.rating=rating
+		if comment != "":
+			res.comment = comment
+	db.session.commit()
+
+def removeRating(custID,prodID):
+	res = Rating.query.filter(custID=custID, prodID = prodID).all()
+	if res != None:
+		db.session.delete(res)
+		db.session.commit()
--- dbtools.py
+++ dbtools.py
@@ -1,84 +1,4 @@
-from tables import *
-
-
-def removeCustomer(id):
-    res = User.query.filter_by(custID=id).first()
-    db.session.delete(res)
-    db.session.commit()
-
-
-def addCustomer(fname, lname, email, pwd):
-    cust = User(fname, lname, email, pwd)
-    db.session.add(cust)
-    db.session.commit()
-
-
-def updateCustomer(id, fname, lname, email, pwd):
-    res = User.query.filter_by(custID=id).first()
-    res.fName = fname
-    res.lName = lname
-    res.emailAdr = email
-    if pwd != '':
-        res.password = pwd
-    db.session.commit()
-
-
-def addProduct(name, price, stock, desc):
-    cust = Product(name, price, stock, desc)
-    db.session.add(cust)
-    db.session.commit()
-
-def removeProduct(id):
-	res = Product.query.filter_by(prodID=id).first()
-	db.session.delete(res)
-	db.session.commit()
-
-def updateProduct(id, name,price,stock,desc):
-	res = Product.query.filter_by(prodID=id).first()
-	res.name = name
-	res.price = price
-	res.curinv = stock
-	if desc != '':
-		res.desc = desc
-	db.session.commit()
-
-def addAddress(street, suite, state, country, zip):
-	adr = Address(street, suite, state, country, zip)
-	db.session.add(adr)
-	db.session.commit()
-
-def removeAddress(id):
-	adr = Address.query.filter_by(adrID=id).first()
-	db.session.delete(adr)
-	db.session.commit()
-
-def updateAddress(id, street, suite, state, country, zip):
-	adr = Address.query.filter_by(adrID=id).first()
-	adr.street=street
-	adr.suite=suite
-	adr.state = state
-	adr.country = country
-	adr.zip = zip
-	db.session.commit()
-
-def addOrder(custID, ShipAdrID):
-	ord = Orders(custID, ShipAdrID)
-	db.session.add(ord)
-	db.session.commit()
-
-def removeOrder(id):
-	res = Orders.query.filter_by(ordID=id).first()
-	db.session.delete(res)
-	db.session.commit()
-
-def updateOrder(id, custID, ShipAdrID):
-	res = Orders.query.filter_by(ordID=id).first()
-	res.custID = custID
-	res.ShipAdrID = ShipAdrID
-	db.session.commit()
-	
-def getOrders(id):
-	return Orders.query.filter(custID=id).all()
+from crud import *
 	
 def addtoCart(custID, prodID, quantity):
 	res = Cart.query.filter_by(custID=custID, prodID=prodID).first()
@@ -107,8 +27,11 @@ def emptyCart(custID):
 		db.session.delete(i)
 	db.session.commit()
 
-def getCart(custID):
-	return Cart.query.filter_by(custID=custID).all()
+def getCartContents(cart):
+	contents = []
+	for i in cart:
+		contents.append(getProduct(i.prodID))
+	return contents
 	
 def checkoutCart(custID, shipAdrID):
 	cart = getCart(custID)
@@ -121,28 +44,6 @@ def checkoutCart(custID, shipAdrID):
 		#Product.query.filter(prodID=i.prodID).first().curinv -= i.quantity
 	db.session.add_all(contents)
 	emptyCart(custID)
-
-def addRating(custID,prodID,rating,comment=""):
-	res = Rating.query.filter(custID=custID, prodID = prodID).all()
-	if res == None:
-		db.session.add(Rating(custID,prodID,rating,comment))
-		db.session.commit()
-		setProdRating(prodID)
-		db.session.commit()
-
-def updateRating(custID,prodID,rating,comment=""):
-	res = Rating.query.filter(custID=custID, prodID = prodID).all()
-	if res != None:
-		res.rating=rating
-		if comment != "":
-			res.comment = comment
-	db.session.commit()
-
-def removeRating(custID,prodID):
-	res = Rating.query.filter(custID=custID, prodID = prodID).all()
-	if res != None:
-		db.session.delete(res)
-		db.session.commit()
 		
 def setProdRating(prodID):
 	res = getRating(prodID)
@@ -150,6 +51,3 @@ def setProdRating(prodID):
 	for i in res:
 		cur += i.rating
 	Product.query.filter(prodID=prodID).first().rating = cur/len(res)
-	
-def getRating(prodID):
-	return Rating.query.filter(prodID = prodID).all()
